5cd387dd670281fad80cb5a0b898c138c7c2391d
Yaremenko Yurii <125539657+YaremenkoYV@users.noreply.github.com>
2023-04-27 23:12:15 +0300

    Create 4.2.py

---
+++ 4.2.py
@@ -0,0 +1,16 @@
+weather = {
+1: {"obsv": "sun", "tmpr": "hotly", "hmdt": "high", "wind": "no"},
+2: {"obsv": "sun", "tmpr": "hotly", "hmdt": "high", "wind": "there is"},
+3: {"obsv": "overcast", "tmpr": "hotly", "hmdt": "high", "wind": "no"}
+}
+print("weather:", weather)
+print("-----------------")
+
+weather1 = weather[1]
+weather2 = weather[2]
+weather3 = weather[3]
+print("weather1:", weather1)
+print("-----------------")
+print("weather2:", weather2)
+print("-----------------")
+print("weather3:", weather3)
